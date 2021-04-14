db600c928518721f0356c281169273dc0ff7ea05
AnfernyReloded <78870003+AnfernyReloded@users.noreply.github.com>
2021-04-14 08:34:53 -0400

    Exercise15

---
+++ Excercise 15/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Excercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            
+                {
+                    while (true)
+                    {
+                        Console.Write("Please Enter a number ");
+                        int userNumber = int.Parse(Console.ReadLine());
+                        int power = 3;
+                        int userIter = 1;
+                        string uChoice;
+
+
+                        for (int i = 1; i <= userNumber; i++)
+                        {
+                            double result = Math.Pow(userIter, power);
+                            Console.WriteLine(result);
+                            userIter++;
+                        }
+
+
+                        Console.WriteLine("Would you like to enter another number?? Y or N");
+                        uChoice = Console.ReadLine().ToLower();
+
+                        if (uChoice == "n")
+                        {
+                            Console.WriteLine("Thank you, Goodbye!");
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+    }
+
